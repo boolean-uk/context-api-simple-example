326f2be088822c56701b1a77637e1a4bc323a6f9
Pia Loana Mullenbach <pia.loana.mullenbach@dk.experis.com>
2024-03-04 09:59:08 +0100

    finished part 1 and 2

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,14 +1,18 @@
-export default function Posts({ posts }) {
-    return (
-        <>
-            {posts.map(post => {
-                return (
-                    <div className="card">
-                        <h3>{post.title}</h3>
-                        <p>{post.content}</p>
-                    </div>
-                )
-            })}
-        </>
-    )
+import { useContext } from "react";
+import { PostsContext } from "../App";
+
+export default function Posts() {
+  const context = useContext(PostsContext);
+  return (
+    <>
+      {context.posts.map((post) => {
+        return (
+          <div className="card">
+            <h3>{post.title}</h3>
+            <p>{post.content}</p>
+          </div>
+        );
+      })}
+    </>
+  );
 }
